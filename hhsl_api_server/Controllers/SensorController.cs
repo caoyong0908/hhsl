33040664ee56c8d25a37187a955a32f23d840ab7
曹勇 <245951161@qq.com>
2021-06-10 22:00:49 +0800

    add del api

--- hhsl_api_server/Controllers/SensorController.cs
+++ hhsl_api_server/Controllers/SensorController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using hhsl_api_server.Response;
+using hhsl_api_server.Sql;
 
 namespace hhsl_api_server.Controllers
 {
@@ -14,6 +16,18 @@ namespace hhsl_api_server.Controllers
         // edit
 
         // del
+        [HttpGet]
+        public ApiResponse Del(int id)
+        {
+            ApiResponse response = new ApiResponse();
+            MySqlOperator opr = new MySqlOperator();
+            opr.Connect();
+            var sql = $"DELETE FROM sensor_info WHERE Id = {id}";
+            opr.Execute(sql);
+            opr.DisConnected();
+            return response;
+
+        }
 
         // list
     }
